dcf3f7c17f78fa003e6f50f0c1028a7e6fba4fae
SRCinVA <scitarella@hotmail.com>
2023-04-25 21:07:52 -0400

    Started to implement gravity

--- tutorial.py
+++ tutorial.py
@@ -11,6 +11,23 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))  # this sets up the window to 
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock  # dictates the speed at which the simulation runs
+    fps = 60 # frames per second
+
+    # for pymunk, you need to create a space
+    space = pymunk.Space()  # this is where we put all our objects
+    space.gravity = (0, 981) # gravity for both x and y direction
 
     while run:
-        clock.tick(60)
+        for event in pygame.event.get(): # this is for any and all pygame events
+            if event.type == pygame.QUIT:  # these enables us to turn the sim off by breaking out of the for loop
+                run = False
+                break
+
+        clock.tick(fps) # regulates the speed
+
+    pygame.quit() # if we fall out of the while loop above, we will (obviously) want to quit.
+
+# to call the run function
+if __name__ == "__main__":
+    run(window, WIDTH, HEIGHT)
+
