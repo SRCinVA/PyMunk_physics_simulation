b19d2d4ef7ec1f91bd51bab7e9abe539a500e4cd
SRCinVA <scitarella@hotmail.com>
2023-04-26 21:43:25 -0400

    Finished draw()

--- tutorial.py
+++ tutorial.py
@@ -8,6 +8,10 @@ pygame.init()
 WIDTH, HEIGHT = 500, 400
 window = pygame.display.set_mode((WIDTH, HEIGHT))  # this sets up the window to view what is going on
 
+def draw(space, window, draw_options): # to manually draw the items in the space
+    window.fill("white") # clear the window by filling out the entire screen
+    space.debug_draw(draw_options)
+
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock  # dictates the speed at which the simulation runs
@@ -17,12 +21,15 @@ def run(window, width, height):
     space = pymunk.Space()  # this is where we put all our objects
     space.gravity = (0, 981) # gravity for both x and y direction
 
+    draw_options = pymunk.pygame_util.DrawOptions(window) # we pass in the pygame window as a surface to draw on
+
     while run:
         for event in pygame.event.get(): # this is for any and all pygame events
             if event.type == pygame.QUIT:  # these enables us to turn the sim off by breaking out of the for loop
                 run = False
                 break
-
+        draw(window, space, draw_options) # need to call draw() here
+        space.step() # how fast you run the sim
         clock.tick(fps) # regulates the speed
 
     pygame.quit() # if we fall out of the while loop above, we will (obviously) want to quit.
