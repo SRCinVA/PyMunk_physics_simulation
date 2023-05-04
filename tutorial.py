c976fa4b07c21d47b24ae6159997e778f113a9d1
SRCinVA <scitarella@hotmail.com>
2023-05-03 21:47:41 -0400

    Finished a boundary

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
+from turtle import position
 import pygame
 import pymunk
 import pymunk.pygame_util # this sets up pygame to draw for pymunk
@@ -25,11 +26,17 @@ def create_ball(space, radius, mass):
 def create_boundaries(space, width, height):
     rects = [ # here we define the rectangles we'll draw onto the screen
             # x and y position of the (a) center of the rectangle and (b) the width and height
-            [(width/2, height - 10), (width, 20)],  # height - 10 so it's flush at the bottom of the screen
-            [(width/2, 10), (width, 20)] # don't understand why this is just 10
-            [(width/2, height - 10), (width, 20)],
-            [(width/2, 10), (width, 20)]
+        [(width/2, height - 10), (width, 20)],  # height - 10 so it's flush at the bottom of the screen
+        [(width/2, 10), (width, 20)], # don't understand why this is just 10
+        [(10, height/2), (20, height)], # not following this ...
+        [(width - 10, height/2), (20, height)]
     ]
+
+    for pos, size in rects: # this is how we draw the rectangles
+        body = pymunk.Body(body_type=pymunk.Body.STATIC) # we want this to not move
+        body.position = pos # after creating the body, then we need to posiiton it
+        shape = pymunk.Poly.create_box(body, size)
+        space.add(body, shape)
 
 def run(window, width, height):
     run = True
@@ -42,6 +49,7 @@ def run(window, width, height):
     space.gravity = (0, 981) # gravity for both x and y direction
 
     ball = create_ball(space, 30, 10)
+    create_boundaries(space, width, height)
 
     draw_options = pymunk.pygame_util.DrawOptions(window) # we pass in the pygame window as a surface to draw on
 
